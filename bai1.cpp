e3da19cb7cf91e8717cfa902bf4eb1f40f979234
fiores <e97520a@gmail.com>
2023-03-11 15:34:13 +0700

    de bai bai 2

--- bai1.cpp
+++ bai1.cpp
@@ -1,11 +1,22 @@
 #include<iostream>
+#include<bits/stdc++.h>
+
 using namespace std;
 
 void input()  {
     int n;
     cin >> n;
     for (int i=0; i<n; i++) {
-        
+        int a=3, b=4;
+        string s;
+        cin >> s;
+        while (s != '*') {
+            if (s == "NUMBER_PEOPLE") {
+                
+            } else if (s == 'NUMBER_PEOPLE_BORN_AT') {
+                
+            }
+        }
     }
 }
 int main() {
